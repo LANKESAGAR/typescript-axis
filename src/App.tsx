043740279050259cc367d74c4dbdd80fe6d5f92c
sagar <sagarl.edu2022@gmail.com>
2025-03-22 14:14:31 -0400

    Crypto Price Chart

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,48 @@ import './App.css';
 import axios from 'axios';
 import CryptoSummary from './components/CryptoSummary';
 import { Crypto } from './CryptoTypes';
+import { Line } from 'react-chartjs-2';
+import type { ChartData, ChartOptions } from 'chart.js';
+import moment from 'moment';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 
 function App() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
   const [selected, setSelected] = useState<Crypto | null>();
+  const [data, setData] = useState<ChartData<'line'>>();
+  const [options, setOptions] = useState<ChartOptions<'line'>>({
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  });
+
   useEffect(() => {
     const url =
       'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
@@ -17,27 +55,49 @@ function App() {
 
   return (
     <>
-  <div className="App">
-    <select onChange={(e)=>{
-      const c = cryptos?.find((x)=> x.id === e.target.value);
-      setSelected(c);
-    }}
-    defaultValue='default'
-    >
-      <option value='default'>Choose an Option</option>
-      {cryptos ?
-        cryptos.map((crypto) => {
-          return <option
-            key={crypto.id} value={crypto.id}>
-            {crypto.name}
-          </option>
-        })
-        : null
-        }
-    </select>
-  </div>
-  {selected ? <CryptoSummary crypto={selected} /> : null }
-  </>
+      <div className="App">
+        <select onChange={(e) => {
+          const c = cryptos?.find((x) => x.id === e.target.value);
+          setSelected(c);
+          axios
+            .get(
+              `https://api.coingecko.com/api/v3/coins/${c?.id}/market_chart?vs_currency=usd&days=30&interval=daily`
+            )
+            .then((response) => {
+              setData({
+                labels: response.data.prices.map((price: number[])=>{
+                  return moment.unix(price[0]/1000).format('MM-DD');
+                }),
+                datasets: [
+                  {
+                    label: 'Dataset 1',
+                    data: response.data.prices.map((price: number[])=>{
+                      return price[1]
+                    }),
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                  },
+                ],
+              })
+            })
+        }}
+          defaultValue='default'
+        >
+          <option value='default'>Choose an Option</option>
+          {cryptos ?
+            cryptos.map((crypto) => {
+              return <option
+                key={crypto.id} value={crypto.id}>
+                {crypto.name}
+              </option>
+            })
+            : null
+          }
+        </select>
+      </div>
+      {selected ? <CryptoSummary crypto={selected} /> : null}
+      {data ? <div style={{width:600}} ><Line options={options} data={data} /></div> : null}
+    </>
   );
 
 }
